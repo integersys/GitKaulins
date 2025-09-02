d2e2c8fbef22c9d62c7d2d3eb17b88cd69f20164
Intars Voitkevičs <intars.voitkevics@sk.lvt.lv>
2025-09-02 12:07:24 +0300

    Ieviests cikls

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -3,12 +3,17 @@ package lvt;
 import java.util.Random;
 
 public class Programma {
-
-	public static void main(String[] args) {
-		int skaitlis;
-		Random rand = new Random();
+	static void mestKaulinu(int reizes) {
+	int skaitlis;
+	Random rand = new Random();
+	for (int i = 1; i<=reizes; i++) {
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
+	}
+	}
+
+	public static void main(String[] args) {
+		mestKaulinu(5);
 		
 	}
 
