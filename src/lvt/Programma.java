0f8ee12c157062b4dc30950cd6348f1626d2b7ff
Intars Voitkevičs <intars.voitkevics@sk.lvt.lv>
2025-09-02 12:15:29 +0300

    Ieviesta iespēja ievadīt metienu skaitu

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -1,15 +1,21 @@
 package lvt;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Programma {
 
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis, reizes;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("67");
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kauliņu?");
+		reizes = scan.nextInt();
+		for (int i=1; i<reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
+		scan.close();
 	}
 
 }
